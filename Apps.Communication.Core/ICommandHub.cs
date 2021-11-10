6a6379bfb1d9c2d527d8c193aebe35fd8ea08e58
Jake Parane <jakes_parane@outlook.com>
2021-11-10 10:43:07 +0800

    hubs update

--- Apps.Communication.Core/ICommandHub.cs
+++ Apps.Communication.Core/ICommandHub.cs
@@ -4,8 +4,8 @@ namespace Apps.Communication.Core
 {
     public interface ICommandHub
     {
-        void RegisterHandler<TCommand, THandler, TResult>(THandler handler) where THandler : IHandle<TCommand, TResult> where TCommand : ICommand;
-        void RegisterValidator<TCommand, TValidator>(TValidator validator) where TValidator : IHandle<TCommand, bool> where TCommand : ICommand;
+        void RegisterHandler<TCommand, THandler, TResult>() where THandler : IHandle<TCommand, TResult> where TCommand : ICommand;
+        void RegisterValidator<TCommand, TValidator>() where TValidator : IHandle<TCommand, bool> where TCommand : ICommand;
         Task<TResult> Dispatch<TCommand, TResult>(TCommand command) where TCommand : ICommand;
     }
 }
--- Apps.Communication.Core/IQueryHub.cs
+++ Apps.Communication.Core/IQueryHub.cs
@@ -4,9 +4,9 @@ namespace Apps.Communication.Core
 {
     public interface IQueryHub
     {
-        void RegisterContributor<TContributor, TQueryResult>(TContributor contributor) 
+        void RegisterContributor<TContributor, TQueryResult>() 
             where TQueryResult : IQueryResult where TContributor : IHandle<TQueryResult>;
-        void RegisterValidator<TValidator, TQuery, TQueryResult>(TValidator validator) where TQuery : IQuery
+        void RegisterValidator<TValidator, TQuery, TQueryResult>() where TQuery : IQuery
             where TQueryResult : IQueryResult where TValidator : IHandle<TQuery, TQueryResult>;
         Task<TQueryResult> Dispatch<TQuery, TQueryResult>(TQuery query) where TQuery : IQuery 
             where TQueryResult : IQueryResult;
